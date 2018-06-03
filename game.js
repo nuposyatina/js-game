573b7af3d2d0a698acd0d4161fb410febabdecf6
Anna Tsukanova <nuposyatina@gmail.com>
2018-06-03 20:18:11 +0300

    Реализован класс Level

--- game.js
+++ game.js
@@ -31,7 +31,7 @@ class Actor {
     }
     this.pos = pos
     this.size = size;
-    this.speed = speed;    
+    this.speed = speed;
   }
 
   act() {
@@ -51,7 +51,7 @@ class Actor {
   }
 
   get bottom() {
-    return this.pos.y + this.soze.y;
+    return this.pos.y + this.size.y;
   }
 
   get type() {
@@ -66,6 +66,86 @@ class Actor {
       return false;
     }
     return actor.bottom > this.top && actor.top < this.bottom && actor.left < this.right && actor.right > this.left;
+  }
+}
+
+class Level {
+  constructor(grid, actors) {
+    this.grid = grid;
+    this.actors = actors;
+    this.player = this.actors.find(actor => actor.type === 'player');
+    this.height = grid.length;
+    this.width = grid.reduce(function (acc, el) {
+      return Math.max(acc, el.length)
+    }, 0)
+    this.status = null;
+    this.finishDelay = 1;
+  }
+
+  isFinished() {
+    return this.status !== null && this.finishDelay < 0;
+  }
+
+  actorAt(actor) {
+    if (!(actor instanceof Actor)) {
+      throw new Error('Должен быть объектом типа Actor');
+    }
+    return this.actors.find(obj => actor.isIntersect(obj));
+  }
+
+  obstacleAt(pos, size) {
+    if (!(pos instanceof Vector) || !(size instanceof Vector)) {
+      throw new Error('Должен быть объектом типа Vector');
+    }
+    let left = Math.floor(pos.x);
+    let right = Math.ceil(pos.x + size.x);
+    let top = Math.floor(pos.y);
+    let bottom = Math.ceil(pos.y + size.y);
+
+    if (top < 0 || left < 0 || right > this.width) {
+      return 'wall';
+    }
+    if (bottom > this.height) {
+      return 'lava';
+    }
+
+    for (i = top; i < bottom; i++) {
+      for (j = left; j < right; j++) {
+        if (this.grid[j][i]) {
+          return this.grid[j][i];
+        }
+      }
+    }
+  }
+
+  removeActor(actor) {
+    let index = this.actors.indexOf(actor);
+    this.actors.splice(index, 1);
+  }
+
+  noMoreActors(type) {
+    if (!(this.actors.find(actor => actor.type === type))) {
+      return true;
+    }
+    return false;
+  }
+
+  playerTouched(type, actor) {
+    if (this.status !== null) {
+      return;
+    }
+
+    if (type === 'lava' || type === 'fireball') {
+      this.status === 'lost';
+      return;
+    }
+
+    if (type === 'coin' && actor.type === 'coin') {
+      this.removeActor(actor);
+      if (this.noMoreActors('coin')) {
+        this.status = 'won';
+      }
+    }
   }
 }
 
